3e734a8548f9b78cdf51741323400fb5f8fc3e95
CocasseOWO <103246783+CocasseOWO@users.noreply.github.com>
2022-06-09 21:02:32 +0200

    update
    - code is now shorter
    - bot show the "is typing" thing when he is working
    - _ibf is now a single message with a single command

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from itertools import product
 import time
 import discord
 from discord.ext import commands
-from webserver import keep_alive
-import os
+#from webserver import keep_alive
+#import os
 
 client = commands.Bot(command_prefix='_', help_command=None, )
 
@@ -14,340 +14,137 @@ async def on_ready():
     print("bot démarré avec le nom de {0.user}".format(client))
 
 
-@client.command()
-async def ibf(ctx):
-    def check(msg):
-        return msg.channel == ctx.channel and msg.author == ctx.author
+# sprint
+def sprint(x):
+    return ((x * 0.6 * 0.91) + (0.1 * (1.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
 
-    await ctx.send('How many ticks do you want to bruteforce ?')
-    inputs = await client.wait_for('message', check=check)
-    inputs = int(inputs.content)
-    if inputs < 10:
-        await ctx.send('What goal speed do you want ?')
-        goal = await client.wait_for('message', check=check)
-        goal = float(goal.content)
-        await ctx.send('What speed precision do you need ?')
-        precision = await client.wait_for('message', check=check)
-        precision = int(precision.content)
 
-        await ctx.send('Running...')
-        start_time = time.time()
+# stop
+def stop(x):
+    return ((x * 0.6 * 0.91) + (0.1 * (0 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
 
-        # sprint
-        def sprint(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (1.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
 
-        # stop
-        def stop(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (0 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
+# walkback
+def walkback(x):
+    return ((x * 0.6 * 0.91) + (0.1 * (1 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
 
-        # walkback
-        def walkback(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (1 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
 
-        # sneak
-        def sneak(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (0.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
+# sneak
+def sneak(x):
+    return ((x * 0.6 * 0.91) + (0.1 * (0.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
 
-        # sneakback
-        def sneakback(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (0.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
 
-        functions_list = [stop, sprint, walkback, sneakback, sneak]
+# sneakback
+def sneakback(x):
+    return ((x * 0.6 * 0.91) + (0.1 * (0.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
 
-        if precision == 5:
-            pr = 0.00001
-        elif precision == 6:
-            pr = 0.000001
-        elif precision == 7:
-            pr = 0.0000001
-        elif precision == 8:
-            pr = 0.00000001
-        elif precision == 9:
-            pr = 0.000000001
-        elif precision == 10:
-            pr = 0.0000000001
-        elif precision == 11:
-            pr = 0.00000000001
-        elif precision == 12:
-            pr = 0.000000000001
-        elif precision == 13:
-            pr = 0.0000000000001
-        elif precision == 14:
-            pr = 0.00000000000001
-        elif precision == 15:
-            pr = 0.000000000000001
-        elif precision == 16:
-            pr = 0.0000000000000001
-        else:
-            await ctx.send('Precision have to be between 5 and 16')
 
-        def functions_product(f, start=0):
-            for func in f:
-                start = func(start)
-            return start
+# walkback45
+def walkback45(x):
+    return ((x * 0.6 * 0.91) + (0.1 * (1 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
 
-        for i in product(functions_list, repeat=int(inputs)):
-            if abs(functions_product(i) - goal) < float(pr):
-                await ctx.send(f'{[f.__name__ for f in i]}: {functions_product(i)}')
-        await ctx.send("--- Results took %s seconds to generate ---" % (time.time() - start_time))
-        await ctx.send('Done !')
 
-    else:
-        await ctx.send('Ticks number have to be between 0 and 9 !, if you want to calculate more ticks, you can run the program in your own computer (https://anonfiles.com/n6C0van5y9/bruteforce_straight_py)')
+# sprint45
+def sprint45(x):
+    return ((x * 0.6 * 0.91) + (0.1 * (1.3 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
+
+
+# sneak45
+def sneak45(x):
+    return ((x * 0.6 * 0.91) + (
+            0.1 * (0.3 * 1.38592929113) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
+
+
+# sneakback45
+def sneakback45(x):
+    return ((x * 0.6 * 0.91) + (
+            0.1 * (0.3 * 1.38592929113) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
+
 
 
 @client.command()
-async def inputbruteforce(ctx):
+async def ibf(ctx, inputs, goal, precision):
     def check(msg):
         return msg.channel == ctx.channel and msg.author == ctx.author
 
-    await ctx.send('How many ticks do you want to bruteforce ?')
-    inputs = await client.wait_for('message', check=check)
-    inputs = int(inputs.content)
-
-    if inputs < 10:
-        await ctx.send('What goal speed do you want ?')
-        goal = await client.wait_for('message', check=check)
-        goal = float(goal.content)
-        await ctx.send('What speed precision do you need ?')
-        precision = await client.wait_for('message', check=check)
-        precision = int(precision.content)
-
-        await ctx.send('Running...')
+    inputs = int(inputs)
+    precision = int(precision)
+    goal = float(goal)
+    await ctx.send('Running...')
+    async with ctx.typing():
         start_time = time.time()
+        rn = 0
+        functions_list = [stop, sprint, walkback, sneakback, sneak,]
 
-        # sprint
-        def sprint(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (1.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
+        if inputs < 10:
 
-        # stop
-        def stop(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (0 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
+            if 6 <= precision <= 16:
+                pr = 1 / 10 ** precision
+            else:
+                await ctx.send('Precision have to be between 6 and 16')
 
-        # walkback
-        def walkback(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (1 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
+            def functions_product(f, start=0):
+                for func in f:
+                    start = func(start)
+                return start
 
-        # sneak
-        def sneak(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (0.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
-
-        # sneakback
-        def sneakback(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (0.3 * 0.98) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
-
-        functions_list = [stop, sprint, walkback, sneakback, sneak]
-
-        if precision == 5:
-            pr = 0.00001
-        elif precision == 6:
-            pr = 0.000001
-        elif precision == 7:
-            pr = 0.0000001
-        elif precision == 8:
-            pr = 0.00000001
-        elif precision == 9:
-            pr = 0.000000001
-        elif precision == 10:
-            pr = 0.0000000001
-        elif precision == 11:
-            pr = 0.00000000001
-        elif precision == 12:
-            pr = 0.000000000001
-        elif precision == 13:
-            pr = 0.0000000000001
-        elif precision == 14:
-            pr = 0.00000000000001
-        elif precision == 15:
-            pr = 0.000000000000001
-        elif precision == 16:
-            pr = 0.0000000000000001
+            for i in product(functions_list, repeat=int(inputs)):
+                if abs(functions_product(i) - goal) < float(pr):
+                    await ctx.send(f'{[f.__name__ for f in i]}: {functions_product(i)}')
+                    rn = rn + 1
+            if rn == 0:
+                await ctx.send(
+                    "--- Results took %s seconds to generate but sadly, nothing was found :(" % (
+                                time.time() - start_time))
+            else:
+                await ctx.send("--- Results took %s seconds to generate" % (time.time() - start_time))
         else:
-            await ctx.send('Precision have to be between 5 and 16')
+            await ctx.send('Ticks number have to be between 0 and 9 !')
+    await ctx.send('Done !')
 
-        def functions_product(f, start=0):
-            for func in f:
-                start = func(start)
-            return start
-
-        for i in product(functions_list, repeat=int(inputs)):
-            if abs(functions_product(i) - goal) < float(pr):
-                await ctx.send(f'{[f.__name__ for f in i]}: {functions_product(i)}')
-        await ctx.send("--- Results took %s seconds to generate ---" % (time.time() - start_time))
-        await ctx.send('Done !')
-    else:
-        await ctx.send('Ticks number have to be between 0 and 9 !, if you want to calculate more ticks, you can run the program in your own computer (https://anonfiles.com/n6C0van5y9/bruteforce_straight_py)')
 
 
 @client.command()
-async def ibf45(ctx):
+async def ibf45(ctx, inputs, goal, precision):
     def check(msg):
         return msg.channel == ctx.channel and msg.author == ctx.author
 
-    await ctx.send('How many ticks do you want to bruteforce ?')
-    inputs = await client.wait_for('message', check=check)
-    inputs = int(inputs.content)
-
-    if inputs < 10:
-        await ctx.send('What goal speed do you want ?')
-        goal = await client.wait_for('message', check=check)
-        goal = float(goal.content)
-        await ctx.send('What speed precision do you need ?')
-        precision = await client.wait_for('message', check=check)
-        precision = int(precision.content)
-
-        await ctx.send('Running...')
+    inputs = int(inputs)
+    precision = int(precision)
+    goal = float(goal)
+    await ctx.send('Running...')
+    async with ctx.typing():
         start_time = time.time()
+        rn = 0
+        functions_list = [stop, sneak45, sneakback45, sprint45, walkback45]
 
-        # sneak45
-        def sneak45(x):
-            return ((x * 0.6 * 0.91) + (
-                    0.1 * (0.3 * 1.38592929113) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
+        if inputs < 10:
 
-        # sneakback45
-        def sneakback45(x):
-            return ((x * 0.6 * 0.91) + (
-                    0.1 * (0.3 * 1.38592929113) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
+            if 6 <= precision <= 16:
+                pr = 1 / 10 ** precision
+            else:
+                await ctx.send('Precision have to be between 6 and 16')
 
-        # sprint45
-        def sprint45(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (1.3 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
+            def functions_product(f, start=0):
+                for func in f:
+                    start = func(start)
+                return start
 
-        # stop
-        def stop(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (0 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
-
-        # walkback45
-        def walkback45(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (1 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
-
-        functions_list = [stop, sprint45, walkback45, sneak45, sneakback45]
-
-        if precision == 5:
-            pr = 0.00001
-        elif precision == 6:
-            pr = 0.000001
-        elif precision == 7:
-            pr = 0.0000001
-        elif precision == 8:
-            pr = 0.00000001
-        elif precision == 9:
-            pr = 0.000000001
-        elif precision == 10:
-            pr = 0.0000000001
-        elif precision == 11:
-            pr = 0.00000000001
-        elif precision == 12:
-            pr = 0.000000000001
-        elif precision == 13:
-            pr = 0.0000000000001
-        elif precision == 14:
-            pr = 0.00000000000001
-        elif precision == 15:
-            pr = 0.000000000000001
-        elif precision == 16:
-            pr = 0.0000000000000001
+            for i in product(functions_list, repeat=int(inputs)):
+                if abs(functions_product(i) - goal) < float(pr):
+                    await ctx.send(f'{[f.__name__ for f in i]}: {functions_product(i)}')
+                    rn = rn + 1
+            if rn == 0:
+                await ctx.send(
+                    "--- Results took %s seconds to generate but sadly, nothing was found :(" % (
+                                time.time() - start_time))
+            else:
+                await ctx.send("--- Results took %s seconds to generate" % (time.time() - start_time))
         else:
-            await ctx.send('Precision have to be between 5 and 16')
-
-        def functions_product(f, start=0):
-            for func in f:
-                start = func(start)
-            return start
-
-        for i in product(functions_list, repeat=int(inputs)):
-            if abs(functions_product(i) - goal) < float(pr):
-                await ctx.send(f'{[f.__name__ for f in i]}: {functions_product(i)}')
-        await ctx.send("--- Results took %s seconds to generate ---" % (time.time() - start_time))
-        await ctx.send('Done !')
-    else:
-        await ctx.send('Ticks number have to be between 0 and 9 !, if you want to calculate more ticks, you can run the program in your own computer (https://anonfiles.com/l5D6v6n4y1/bruteforce_45_py)')
+            await ctx.send('Ticks number have to be between 0 and 9 !')
+    await ctx.send('Done !')
 
 
-@client.command()
-async def inputbruteforce45(ctx):
-    def check(msg):
-        return msg.channel == ctx.channel and msg.author == ctx.author
-
-    await ctx.send('How many ticks do you want to bruteforce ?')
-    inputs = await client.wait_for('message', check=check)
-    inputs = int(inputs.content)
-
-    if inputs < 10:
-        await ctx.send('What goal speed do you want ?')
-        goal = await client.wait_for('message', check=check)
-        goal = float(goal.content)
-        await ctx.send('What speed precision do you need ?')
-        precision = await client.wait_for('message', check=check)
-        precision = int(precision.content)
-
-        await ctx.send('Running...')
-        start_time = time.time()
-
-        # sneak45
-        def sneak45(x):
-            return ((x * 0.6 * 0.91) + (
-                    0.1 * (0.3 * 1.38592929113) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
-
-        # sneakback45
-        def sneakback45(x):
-            return ((x * 0.6 * 0.91) + (
-                    0.1 * (0.3 * 1.38592929113) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
-
-        # sprint45
-        def sprint45(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (1.3 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
-
-        # stop
-        def stop(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (0 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / 0.6) ** 3))
-
-        # walkback45
-        def walkback45(x):
-            return ((x * 0.6 * 0.91) + (0.1 * (1 * 1) * ((1 + 0.2 * 0) * (1 - 0.15 * 0)) * (0.6 / -0.6) ** 3))
-
-        functions_list = [stop, sprint45, walkback45, sneak45, sneakback45]
-
-        if precision == 5:
-            pr = 0.00001
-        elif precision == 6:
-            pr = 0.000001
-        elif precision == 7:
-            pr = 0.0000001
-        elif precision == 8:
-            pr = 0.00000001
-        elif precision == 9:
-            pr = 0.000000001
-        elif precision == 10:
-            pr = 0.0000000001
-        elif precision == 11:
-            pr = 0.00000000001
-        elif precision == 12:
-            pr = 0.000000000001
-        elif precision == 13:
-            pr = 0.0000000000001
-        elif precision == 14:
-            pr = 0.00000000000001
-        elif precision == 15:
-            pr = 0.000000000000001
-        elif precision == 16:
-            pr = 0.0000000000000001
-        else:
-            await ctx.send('Precision have to be between 5 and 16')
-
-        def functions_product(f, start=0):
-            for func in f:
-                start = func(start)
-            return start
-
-        for i in product(functions_list, repeat=int(inputs)):
-            if abs(functions_product(i) - goal) < float(pr):
-                await ctx.send(f'{[f.__name__ for f in i]}: {functions_product(i)}')
-        await ctx.send("--- Results took %s seconds to generate ---" % (time.time() - start_time))
-        await ctx.send('Done !')
-    else:
-        await ctx.send('Ticks number have to be between 0 and 9 !, if you want to calculate more ticks, you can run the program in your own computer (https://anonfiles.com/l5D6v6n4y1/bruteforce_45_py)')
 
 @client.command()
 async def help(ctx):
@@ -361,6 +158,8 @@ async def help(ctx):
     await ctx.send(embed=embed)
 
 
-keep_alive()
+
+
+#keep_alive()
 
 client.run('TOKEN_HERE')
